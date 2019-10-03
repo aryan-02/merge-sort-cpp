1f9de1887ea149517100ea0d62e3d874f0764bfa
Aryan Mediratta <aryan_02@outlook.com>
2019-10-03 22:18:28 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -4,21 +4,25 @@
 
 template<typename T>
 std::vector<T> merge(const std::vector<T>& a, const std::vector<T>& b) 
-{ // Merges two already sorted vectors into a combined sorted vector
-	int iter_a{};
-	int iter_b{};
+{ 
+	// Merges two already sorted vectors into a combined sorted vector
+	int iter_a{}; // Iterates over all elements of a for merge
+	int iter_b{}; // Iterates over all elements of b for merge
 	const unsigned int size_a{a.size()};
 	const unsigned int size_b{b.size()};
 	std::vector<T> output{};
 	while (size_a + size_b - iter_a - iter_b > 0)
 	{
+		// Compares the current elements of a and b, and appends the smaller one to output
 		if (iter_a == size_a)
 		{
+			// The case where all elements of a are in the output vector
 			output.push_back(b.at(iter_b));
 			iter_b++;
 		}
 		else if (iter_b == size_b)
 		{
+			// The case where all elements of b are in the output vector
 			output.push_back(a.at(iter_a));
 			iter_a++;
 		}
@@ -50,8 +54,8 @@ std::vector<T> msort(const std::vector<T>& input)
 		return input; // Base case, a vector with one element is sorted by definition
 	}
 	else {
-		std::vector<T> left(input.begin(), input.begin() + (input.size() / 2));
-		std::vector<T> right(input.begin() + (input.size() / 2), input.end());
+		std::vector<T> left(input.begin(), input.begin() + (input.size() / 2)); // Left half
+		std::vector<T> right(input.begin() + (input.size() / 2), input.end()); // Right half
 		return merge(msort(left), msort(right)); // Recursively break the vector into two halves and merge
 	}
 }
@@ -79,7 +83,7 @@ int main()
 	}
 	std::vector<int> f{msort(a)};
 	std::cout << "Sorted List:" << '\n';
-	for (int i : f)
+	for (const int& i : f)
 	{
 		std::cout << i << ' ';
 	}
